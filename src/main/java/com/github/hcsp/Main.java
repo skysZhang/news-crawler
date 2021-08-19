f24bb50b8564da1d43e673995fdc17db1a416823
skysZhang <37018639+skysZhang@users.noreply.github.com>
2021-08-19 16:04:27 +0800

    设计并实现基本的算法 (#1)
    * 设计并实现基本的算法
    
    这个提交中，我们实现了基本的爬虫算法
    虽然它还非常原始，但是我们想要小步提交
    
    * 修复CI检查错误
    
    one checkstyle violation
    
    Co-authored-by: skyszhang <1296003529@qq.com>

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -6,19 +6,95 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            HttpGet httpGet = new HttpGet("https://sina.cn/");
-            try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
-                System.out.println(response1.getStatusLine());
-                HttpEntity entity1 = response1.getEntity();
-                System.out.println(EntityUtils.toString(entity1));
+
+        // 待处理的链接池
+        List<String> linkPool = new ArrayList<>();
+        // 已经处理的链接池
+        Set<String> processdLinks = new HashSet<>();
+        linkPool.add("https://sina.cn");
+
+        while (true) {
+            if (linkPool.isEmpty()) {
+                break;
+            }
+            String link = linkPool.remove(linkPool.size() - 1);
+
+            if (processdLinks.contains(link)) {
+                continue;
+            }
+
+            if (isInterestingLink(link)) {
+                Document doc = httpGetAndParseHtml(link);
+
+                doc.select("a").stream().map(aTag -> aTag.attr("href")).forEach(linkPool::add);
+
+                // 假如这是一个新闻的详情页面，就存入数据库，否则，就什么都不做
+                storeIntoDatabaseIfItIsNewsPage(doc);
+                processdLinks.add(link);
+            } else {
+                //这是我们不感兴趣的，我们不处理它
             }
         }
+    }
+
+    private static void storeIntoDatabaseIfItIsNewsPage(Document doc) {
+        ArrayList<Element> articleTags = doc.select("article");
+        if (!articleTags.isEmpty()) {
+            for (Element articleTag : articleTags) {
+                String title = articleTags.get(0).child(0).text();
+                System.out.println(title);
+            }
+        }
+    }
+
+    private static Document httpGetAndParseHtml(String link) throws IOException {
+        //这是我们感兴趣的，我们只处理新浪站内的链接
+        if (isInterestingLink(link)) {
+
+            try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+                System.out.println(link);
+                if (link.startsWith("//")) {
+                    link = "https:" + link;
+                }
+                HttpGet httpGet = new HttpGet(link);
+                httpGet.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36");
+
+                try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+                    System.out.println(response1.getStatusLine());
+                    HttpEntity entity1 = response1.getEntity();
+                    String html = EntityUtils.toString(entity1);
+                }
+            }
+        }
+        return Jsoup.parse(link);
+    }
+
+    private static boolean isInterestingLink(String link) {
+        return (isNewsPage(link) || isIndexPage(link)) && isNotLoginPage(link);
+    }
+
+    private static boolean isIndexPage(String link) {
+        return "https://sina.cn".equals(link);
+    }
+
+    private static boolean isNewsPage(String link) {
+        return link.contains("news.sina.cn");
+    }
+
+    private static boolean isNotLoginPage(String link) {
+        return !link.contains("passport.sina.cn");
     }
 }
 
